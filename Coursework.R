b88ccaec7a4afe26902b4171d21f2310c5531ec4
refracc <40345422@live.napier.ac.uk>
2020-10-10 10:08:03 +0100

    Auto-generated push message from refracc on MacBook-Air.local

--- Coursework.R
+++ Coursework.R
@@ -3,18 +3,18 @@ setwd("/users/refracc/Documents/R/SET09120/Coursework")
 
 data <- read.csv("dataset.csv")
 
-# Potential outliers plot
+# Definite outliers plot
 outliers <- ggplot(data, aes(x=Wind.Direction, y=Height, fill=Gender)) + geom_boxplot() + facet_wrap(~Group) + xlab("Wind Direction") + ylab("Height (meters)") + labs(title = "Outliers 1")
 print(outliers)
-
 
 # Definite relationship plot
 relationship <- ggplot(data, aes(x=Wind.Direction, y=Offset, fill=Gender)) + geom_boxplot() + facet_wrap(~Group) + xlab("Wind Direction") + labs(title = "Relationship - Gender & Offset")
 print(relationship)
 
-# Potential relationship plot
+# Definite relationship plot
 relationship1 <- ggplot(data, aes(x=Angle, y=Offset, fill=Gender)) + geom_boxplot() + facet_wrap(~Group) + xlab("Angle (degrees)") + labs(title = "Relationship - Angle & Offset")
 print(relationship1)
 
-relationship2 <- ggplot(data, aes(x=Distance, y=Height, fill=Wind.Direction)) + geom_point() + facet_wrap(~Group)
+# Definite relationship plot
+relationship2 <- ggplot(data[data$Height < 100, ], aes(x=Wind.Direction, y=Height, fill=Gender)) + geom_boxplot() + facet_wrap(~Group) + xlab("Wind Direction") + ylab("Height (meters)") + labs(title = "Relationship - Wind Direction & Height")
 print(relationship2)
