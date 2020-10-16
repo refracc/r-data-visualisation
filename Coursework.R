1090393fab87672277e5291fefd06b586b2764f1
refracc <40345422@live.napier.ac.uk>
2020-10-16 19:47:57 +0100

    Auto-generated push message from refracc on MacBook-Air.local

--- Coursework.R
+++ Coursework.R
@@ -3,22 +3,23 @@ setwd("/users/refracc/Documents/R/SET09120/Coursework")
 
 data <- read.csv("dataset.csv")
 
-# Definite outliers plot
-outliers <- ggplot(data, aes(x=Wind.Direction, y=Height, fill=Gender)) + geom_boxplot() + facet_wrap(~Group) + xlab("Wind Direction") + ylab("Height (meters)") + labs(title = "Outliers 1")
+# Definite outliers plot (global)
+outliers <- ggplot(data, aes(x=Wind.Direction, y=Height, fill=Gender)) + geom_boxplot() + facet_grid(~Group) + xlab("Wind Direction") + ylab("Height (meters)") + labs(title = "Global Outliers")
 print(outliers)
 
+# Definite outliers plot (contextual)
 # Definite relationship plot
-relationship <- ggplot(data, aes(x=Wind.Direction, y=Offset, fill=Gender)) + geom_boxplot() + facet_wrap(~Group) + xlab("Wind Direction") + labs(title = "Relationship - Gender & Offset")
+rel <- ggplot(data, aes(x=Angle, y=Distance, color=Group, shape=Group)) + geom_point(alpha = 0.7) + labs(title="Relationship - Angle & Distance (Contextual Outlier)") + xlab("Angle (degrees)") + ylab("Distance (meters)") + scale_fill_discrete(name = "Group", labels = c("Experienced", "Novice", "Professional", "Very Experienced"))
+print(rel)
+
+# Definite relationship plot
+relationship <- ggplot(data, aes(x=Wind.Direction, y=Offset, fill=Gender)) + geom_boxplot() + facet_grid(~Group) + xlab("Wind Direction") + labs(title = "Relationship - Wind Direction & Offset") + scale_fill_discrete(name = "Gender", labels = c("Female", "Male"))
 print(relationship)
 
 # Definite relationship plot
-relationship1 <- ggplot(data, aes(x=Angle, y=Offset, fill=Gender)) + geom_boxplot() + facet_wrap(~Group) + xlab("Angle (degrees)") + labs(title = "Relationship - Angle & Offset")
+relationship1 <- ggplot(data, aes(x=Angle, y=Offset, fill=Gender)) + geom_boxplot() + facet_grid(~Group) + xlab("Angle (degrees)") + ylab("Offset (cm)") + labs(title = "Relationship - Angle & Offset") + scale_fill_discrete(name = "Gender", labels = c("Female", "Male"))
 print(relationship1)
 
 # Definite relationship plot
-relationship2 <- ggplot(data[data$Height < 100, ], aes(x=Wind.Direction, y=Height, fill=Gender)) + geom_boxplot() + facet_wrap(~Group) + xlab("Wind Direction") + ylab("Height (meters)") + labs(title = "Relationship - Wind Direction & Height")
-print(relationship2)
-
-# Definite relationship plot
-relationship3 <- ggplot(data, aes(x=Score.1, y=Score.2, fill=Wind.Direction)) + geom_boxplot() + facet_grid(Gender~Group) + xlab("Score 1") + ylab("Score 2") + scale_fill_discrete(name="Wind Direction") + labs(title="Relationship - Score 1 & Score 2")
-print(relationship3)
+re <- ggplot(data, aes(x=Score.2, y=Age, color=Group, shape=Group)) + geom_point(alpha = 0.7) + facet_grid(Gender~Wind.Direction)  + scale_fill_discrete(name = "Group", labels = c("Experienced", "Novice", "Professional", "Very Experienced")) + xlab("Score 2") + labs(title = "Relationship - Age & Score.2")
+print(re)
